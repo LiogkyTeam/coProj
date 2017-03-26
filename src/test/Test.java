7f91be896703badef6530b0c6efb5d32c7597306
AlexLiogky <al.liogky@yandex.ru>
2017-03-27 00:05:01 +0300

    Hey (#6)
    * first commit

---
+++ src/test/Test.java
@@ -0,0 +1,10 @@
+package test;
+
+/**
+ * Created by Alexey on 26.03.2017.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hey Pes, you");
+    }
+}
